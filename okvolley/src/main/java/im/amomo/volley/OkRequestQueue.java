0ef290ec8b13272883d0e644f8a591bac6489965
Zoran Smilevski <zoran.smilevski@wpn.tv>
2015-06-19 13:54:34 +0200

    *commit bug :)

--- okvolley/src/main/java/im/amomo/volley/OkRequestQueue.java
+++ okvolley/src/main/java/im/amomo/volley/OkRequestQueue.java
@@ -1,8 +1,42 @@
 package im.amomo.volley;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+
+import java.util.Map;
+
 /**
  * Created by zoki on 17/06/15.
  */
-public class OkRequestQueue
+public class OkRequestQueue extends RequestQueue
 {
+	private Map<String, String> mRequestHeaders = null;
+
+	public OkRequestQueue(Cache cache, Network network)
+	{
+		super(cache, network);
+	}
+
+	public OkRequestQueue(Cache cache, Network network, Map<String, String> mRequestHeaders)
+	{
+		super(cache, network);
+	}
+
+	public OkRequestQueue updateRequestHeaders(Map<String, String> requestHeaders)
+	{
+		mRequestHeaders = requestHeaders;
+		return this;
+	}
+
+	@Override
+	public <T> Request<T> add(Request<T> request)
+	{
+		if (request instanceof OkRequest && mRequestHeaders != null)
+		{
+			((OkRequest)request).headers(mRequestHeaders);
+		}
+		return super.add(request);
+	}
 }
